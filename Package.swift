30145373a29090681a73442b65041402ea1d48cb
atacan <atacandur@icloud.com>
2023-07-22 13:43:23 +0200

    on change of reducer

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TCAEnchance",
+    platforms: [
+      .iOS(.v13),
+      .macOS(.v10_15),
+      .tvOS(.v13),
+      .watchOS(.v6),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "TCAEnchance",
+            targets: ["TCAEnchance"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.49.1"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "TCAEnchance",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]),
+        .testTarget(
+            name: "TCAEnchanceTests",
+            dependencies: ["TCAEnchance"]),
+    ]
+)
